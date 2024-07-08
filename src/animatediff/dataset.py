Added webdataset and fixed gradient accumulation

src/animatediff/dataset.py
@@ -3,98 +3,76 @@ import io
 import math
 import os
 import random
+import sys
+from functools import partial
 
 import numpy as np
 import torch
 import torchvision.transforms as transforms
 from decord import VideoReader
-from einops import rearrange
+from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Dataset
 
+sys.path.append("./webdataset/")
+import wids as wids
 
-class WebVid10M(Dataset):
-    def __init__(
-            self,
-            csv_path, video_folder,
-            sample_size=256, sample_stride=4, sample_n_frames=16,
-            is_image=False,
-        ):
-        print(f"loading annotations from {csv_path} ...")
-        with open(csv_path, 'r') as csvfile:
-            self.dataset = list(csv.DictReader(csvfile))
-        self.length = len(self.dataset)
-        print(f"data scale: {self.length}")
+import webdataset as wds
 
-        self.video_folder    = video_folder
-        self.sample_stride   = sample_stride
-        self.sample_n_frames = sample_n_frames
-        self.is_image        = is_image
 
-        sample_size = tuple(sample_size) if not isinstance(sample_size, int) else (sample_size, sample_size)
-        self.pixel_transforms = transforms.Compose([
-            # transforms.RandomHorizontalFlip(),
-            transforms.Resize(sample_size[0]),
-            transforms.CenterCrop(sample_size),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], inplace=True),
-        ])
+def make_sample(sample, sample_size=256, target_fps=8, sample_n_frames=16, is_image=False, **kwargs):
+    try:
+        video_path = sample[".mp4"]
+        caption = sample[".txt"]
 
-    def get_batch(self, idx):
-        video_dict = self.dataset[idx]
-        videoid, name, page_dir = video_dict['videoid'], video_dict['name'], video_dict['page_dir']
-
-        video_dir    = os.path.join(self.video_folder, f"{videoid}.mp4")
-        video_reader = VideoReader(video_dir)
+        video_reader = VideoReader(video_path)
         video_length = len(video_reader)
 
-        if not self.is_image:
-            clip_length = min(video_length, (self.sample_n_frames - 1) * self.sample_stride + 1)
-            start_idx   = random.randint(0, video_length - clip_length)
-            batch_index = np.linspace(start_idx, start_idx + clip_length - 1, self.sample_n_frames, dtype=int)
-        else:
+        if video_length == 0:
+            raise ValueError("Empty video file")
+
+        original_fps = video_reader.get_avg_fps()
+
+        if is_image:
             batch_index = [random.randint(0, video_length - 1)]
+        else:
+            frame_interval = original_fps / target_fps
+            total_duration = (sample_n_frames - 1) / target_fps
+            max_start_frame = max(0, video_length - int(total_duration * original_fps) - 1)
+            start_frame = random.randint(0, max_start_frame)
+            batch_index = [round(start_frame + i * frame_interval) for i in range(sample_n_frames)]
+            batch_index = [min(i, video_length - 1) for i in batch_index]  # Ensure we don't exceed video length
 
         pixel_values = torch.from_numpy(video_reader.get_batch(batch_index).asnumpy()).permute(0, 3, 1, 2).contiguous()
         pixel_values = pixel_values / 255.
         del video_reader
 
-        if self.is_image:
+        if is_image:
             pixel_values = pixel_values[0]
 
-        return pixel_values, name
+        sample_size = tuple(sample_size) if not isinstance(sample_size, int) else (sample_size, sample_size)
+        pixel_transforms = transforms.Compose([
+            transforms.Resize(sample_size[0]),
+            transforms.CenterCrop(sample_size),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], inplace=True),
+        ])
 
-    def __len__(self):
-        return self.length
+    except KeyError as e:
+        print(f"Missing key in sample: {e}")
+        return None
+    except (FileNotFoundError, RuntimeError, ValueError) as e:
+        print(f"Error processing sample: {e}")
+        return None
 
-    def __getitem__(self, idx):
-        while True:
-            try:
-                pixel_values, name = self.get_batch(idx)
-                break
+    return dict(pixel_values=pixel_transforms(pixel_values), text=caption)
 
-            except Exception as e:
-                idx = random.randint(0, self.length-1)
+def make_dataset(shards, cache_dir="./tmp", **kwargs):
+    trainset = wids.ShardListDataset(shards, cache_dir=cache_dir, keep=True)
+    trainset = trainset.add_transform(partial(make_sample, **kwargs))
+    return trainset
 
-        pixel_values = self.pixel_transforms(pixel_values)
-        sample = dict(pixel_values=pixel_values, text=name)
-        return sample
-
-
-
-if __name__ == "__main__":
-    from animatediff.utils.util import save_videos_grid
-
-    dataset = WebVid10M(
-        csv_path="/mnt/petrelfs/guoyuwei/projects/datasets/webvid/results_2M_val.csv",
-        video_folder="/mnt/petrelfs/guoyuwei/projects/datasets/webvid/2M_val",
-        sample_size=256,
-        sample_stride=4, sample_n_frames=16,
-        is_image=True,
+def make_dataloader(dataset, batch_size=1):
+    sampler = wids.DistributedChunkedSampler(dataset, chunksize=1000, shuffle=True)
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, sampler=sampler, num_workers=1
     )
-    import pdb
-    pdb.set_trace()
-
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, num_workers=16,)
-    for idx, batch in enumerate(dataloader):
-        print(batch["pixel_values"].shape, len(batch["text"]))
-        # for i in range(batch["pixel_values"].shape[0]):
-        #     save_videos_grid(batch["pixel_values"][i:i+1].permute(0,2,1,3,4), os.path.join(".", f"{idx}-{i}.mp4"), rescale=True)
+    return dataloader

src/animatediff/train.py
@@ -15,7 +15,6 @@ import torch
 import torch.distributed as dist
 import torch.nn.functional as F
 import torchvision
-import wandb
 from diffusers import AutoencoderKL, StableDiffusionPipeline
 from diffusers.models import UNet2DConditionModel
 from diffusers.optimization import get_scheduler as get_lr_scheduler
@@ -27,7 +26,8 @@ from torch.utils.data.distributed import DistributedSampler
 from tqdm.auto import tqdm
 from transformers import CLIPImageProcessor, CLIPTokenizer
 
-from animatediff.dataset import WebVid10M
+import wandb
+from animatediff.dataset import make_dataloader, make_dataset
 from animatediff.models.clip import CLIPSkipTextModel
 from animatediff.models.unet import UNet3DConditionModel
 from animatediff.pipelines.animation import AnimationPipeline
@@ -53,10 +53,10 @@ def zero_rank_print(s, logtype:LogType = LogType.info):
 
 def init_dist(launcher="slurm", backend='gloo', port=29500, **kwargs):
     """Initializes distributed environment."""
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['WORLD_SIZE'] = '1'
-    os.environ['RANK'] = '0'
-    os.environ['MASTER_PORT'] = str(port)
+    # os.environ['MASTER_ADDR'] = 'localhost'
+    # os.environ['WORLD_SIZE'] = '1'
+    # os.environ['RANK'] = '0'
+    # os.environ['MASTER_PORT'] = str(port)
     try:
         if launcher == 'pytorch':
             rank = int(os.environ['RANK'])
@@ -231,6 +231,7 @@ def train_ad(
 
     # Enable gradient checkpointing
     if gradient_checkpointing:
+        logger.info("Enabling gradient checkpointing")
         unet.enable_gradient_checkpointing()
 
     # Move models to GPU
@@ -245,38 +246,38 @@ def train_ad(
     vae = vae.to(device=local_rank)
 
     # Get the training dataset
-    # train_dataset = make_dataset(**train_data)
-    # train_dataloader = make_dataloader(train_dataset, batch_size=train_batch_size)
+    train_dataset = make_dataset(**train_data)
+    train_dataloader = make_dataloader(train_dataset, batch_size=train_batch_size)
 
-    # Get the training dataset
-    train_dataset = WebVid10M(**train_data, is_image=image_finetune)
-    distributed_sampler = DistributedSampler(
-        train_dataset,
-        num_replicas=num_processes,
-        rank=global_rank,
-        shuffle=True,
-        seed=global_seed,
-    )
+    # # Get the training dataset
+    # train_dataset = WebVid10M(**train_data, is_image=image_finetune)
+    # distributed_sampler = DistributedSampler(
+    #     train_dataset,
+    #     num_replicas=num_processes,
+    #     rank=global_rank,
+    #     shuffle=True,
+    #     seed=global_seed,
+    # )
 
-    # DataLoaders creation:
-    train_dataloader = torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=train_batch_size,
-        shuffle=False,
-        sampler=distributed_sampler,
-        num_workers=num_workers,
-        pin_memory=True,
-        drop_last=True,
-    )
+    # # DataLoaders creation:
+    # train_dataloader = torch.utils.data.DataLoader(
+    #     train_dataset,
+    #     batch_size=train_batch_size,
+    #     shuffle=False,
+    #     sampler=distributed_sampler,
+    #     num_workers=num_workers,
+    #     pin_memory=True,
+    #     drop_last=True,
+    # )
 
     # Get the training iteration
     if max_train_steps == -1:
         assert max_train_epoch != -1
-        max_train_steps = max_train_epoch * len(train_dataloader)
+        max_train_steps = max_train_epoch * len(train_dataset)
 
     if checkpointing_steps == -1:
         assert checkpointing_epochs != -1
-        checkpointing_steps = checkpointing_epochs * len(train_dataloader)
+        checkpointing_steps = checkpointing_epochs * len(train_dataset)
 
     if scale_lr:
         learning_rate = (learning_rate * gradient_accumulation_steps * train_batch_size * num_processes)
@@ -291,12 +292,12 @@ def train_ad(
 
     # DDP wrapper
     unet_single = unet.to(device=local_rank)
+    # unet_single = torch.compile(unet_single)
     unet = DDP(unet_single, device_ids=[local_rank], output_device=local_rank)
 
     # We need to recalculate our total training steps as the size of the training dataloader may have changed.
-    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / gradient_accumulation_steps)
+    num_update_steps_per_epoch = math.ceil(len(train_dataset) / gradient_accumulation_steps)
     # Afterwards we recalculate our number of training epochs
-    zero_rank_print(f"{max_train_steps} {num_update_steps_per_epoch} {len(train_dataloader)}")
     num_train_epochs = math.ceil(max_train_steps / num_update_steps_per_epoch)
 
     # Train!
@@ -344,7 +345,7 @@ def train_ad(
                         torchvision.utils.save_image(pixel_value, f"{output_dir}/sanity_check/{'-'.join(text.replace('/', '').split()[:10]) if not text == '' else f'{global_rank}-{idx}'}.png")
 
             # Periodically validation
-            if is_main_process and (global_step % validation_steps == 0 or global_step in validation_steps_tuple):
+            if is_main_process and (global_step in validation_steps_tuple):
                 zero_rank_print("Validation")
 
                 # Validation pipeline
@@ -433,7 +434,6 @@ def train_ad(
             # Add noise to the latents according to the noise magnitude at each timestep
             # (this is the forward diffusion process)
             noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
-            del latents
 
             zero_rank_print("Get text embedding", LogType.debug)
             # Get the text embedding for conditioning
@@ -443,6 +443,9 @@ def train_ad(
                 ).input_ids.to(local_rank)
                 encoder_hidden_states = text_encoder(prompt_ids)[0]
 
+            del latents, batch
+            torch.cuda.empty_cache()
+
             # Get the target for loss depending on the prediction type
             if noise_scheduler.config.prediction_type == "epsilon":
                 target = noise
@@ -451,56 +454,70 @@ def train_ad(
             else:
                 raise ValueError(f"Unknown prediction type {noise_scheduler.config.prediction_type}")
 
+            # Reset gradients at the beginning of the accumulation cycle
+            if step % gradient_accumulation_steps == 0:
+                zero_rank_print(f"Reset gradients at the beginning of the accumulation cycle", LogType.debug)
+                optimizer.zero_grad()
+
             # Predict the noise residual and compute loss
             # Mixed-precision training
             zero_rank_print(f"Predict the noise residual and compute loss Mixed Precision: {mixed_precision_training}", LogType.debug)
             with torch.cuda.amp.autocast(enabled=mixed_precision_training):
                 model_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
                 loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
+                loss = loss / gradient_accumulation_steps  # Normalize loss to account for accumulation
 
-                optimizer.zero_grad()
-                zero_rank_print("Backpropagate", LogType.debug)
+            del noisy_latents, noise, timesteps, model_pred
+            torch.cuda.empty_cache()
 
+            # Backpropagate loss, accumulate gradients
+            zero_rank_print("Backpropagate", LogType.debug)
+            if mixed_precision_training:
+                scaler.scale(loss).backward()  # Scale the loss; backward pass accumulates gradients
+            else:
+                loss.backward()  # Gradient accumulation without scaling
+
+            # Apply the optimizer step and update the learning rate scheduler only at the end of an accumulation period or at the last batch
+            if (step + 1) % gradient_accumulation_steps == 0 or step == len(train_dataset) - 1:
+                zero_rank_print("=== Accumulate gradients", LogType.debug)
                 if mixed_precision_training:
-                    scaler.scale(loss).backward()
-                    scaler.unscale_(optimizer)
+                    scaler.unscale_(optimizer)  # Unscale gradients before clipping
                     torch.nn.utils.clip_grad_norm_(unet.parameters(), max_grad_norm)
-                    scaler.step(optimizer)
-                    scaler.update()
+                    scaler.step(optimizer)  # Perform optimizer step
+                    scaler.update()  # Update the scale for next iteration
                 else:
-                    loss.backward()
                     torch.nn.utils.clip_grad_norm_(unet.parameters(), max_grad_norm)
                     optimizer.step()
 
-            lr_scheduler.step()
+                lr_scheduler.step()  # Update learning rate
+
             progress_bar.update(1)
             global_step += 1
-
             ### <<<< Training <<<< ###
 
-            # Wandb logging
-            if is_main_process and (not is_debug) and use_wandb:
-                wandb.log({"train_loss": loss.item()}, step=global_step)
-
             # Save checkpoint
-            if is_main_process and (global_step % checkpointing_steps == 0 or step == len(train_dataloader) - 1):
+            if is_main_process and (global_step % checkpointing_steps == 0 or step == len(train_dataset) - 1):
                 save_path = os.path.join(output_dir, f"checkpoints")
                 state_dict = {
                     "epoch": epoch,
                     "global_step": global_step,
                     "state_dict": unet.state_dict(),
                 }
-                if step == len(train_dataloader) - 1:
+                if step == len(train_dataset) - 1:
                     torch.save(state_dict, os.path.join(save_path, f"checkpoint-epoch-{epoch+1}.ckpt"))
                 else:
                     torch.save(state_dict, os.path.join(save_path, f"checkpoint.ckpt"))
                 logging.info(f"Saved state to {save_path} (global_step: {global_step})")
 
+            # Logging
             logs = {"step_loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
             progress_bar.set_postfix(**logs)
 
-            # Clear memory
-            del batch, model_pred, loss, noisy_latents, noise, timesteps
+            # Wandb logging
+            if is_main_process and (not is_debug) and use_wandb:
+                wandb.log({"train_loss": (loss * gradient_accumulation_steps).item()}, step=global_step)
+
+            del loss
             torch.cuda.empty_cache()
 
             if global_step >= max_train_steps: