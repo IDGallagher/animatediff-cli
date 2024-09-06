dcabeab9e215ab9590064a09926476499b642ebb
IDGallagher <idgallagher@gmail.com>
2024-09-06 15:39:38 +0800

    .

--- src/training/train_ad.py
+++ src/training/train_ad.py
@@ -451,8 +451,8 @@ def train_ad(
 
             # Periodically validation
             actual_steps = global_step/gradient_accumulation_steps
-            # if is_main_process and actual_steps in validation_steps_tuple or actual_steps % validation_steps == 0:
-            if False:
+            if is_main_process and actual_steps in validation_steps_tuple or actual_steps % validation_steps == 0:
+            # if False:
                 zero_rank_print("Validation")
 
                 # Validation pipeline
