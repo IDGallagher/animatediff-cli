752e4cbd39bb11ed1229651c0e5edbfd9635df92
IDGallagher <idgallagher@gmail.com>
2024-09-06 14:00:47 +0800

    Moved model prediction to own function in preparation for val tracking

--- src/training/train_ad.py
+++ src/training/train_ad.py
@@ -46,6 +46,111 @@ from .utils import LogType, apply_lora, zero_rank_partial
 logger = logging.getLogger(__name__)
 zero_rank_print: Callable[[str, LogType], None] = partial(zero_rank_partial, logger)
 
+# actual prediction function - shared between train and validate
+def get_model_prediction_and_target(batch, unet, vae, noise_scheduler, tokenizer, text_encoder, zero_frequency_noise_ratio=0.0, return_loss=False, loss_scale=None, embedding_perturbation=0.0, mixed_precision_training: bool = True, image_finetune: bool = False):
+    with torch.no_grad():
+        with torch.autocast('cuda', enabled=mixed_precision_training):
+            pixel_values = batch[0].to(unet.device)
+            texts = batch[1]
+            pixel_values = normalize_and_rescale(pixel_values)
+            zero_rank_print("Convert videos to latent space", LogType.debug)
+            if not image_finetune:
+                pixel_values = rearrange(pixel_values, "b f c h w -> (b f) c h w")
+                latents = vae.encode(pixel_values).latent_dist
+                latents = latents.sample()
+            else:
+                latents = vae.encode(pixel_values).latent_dist
+                latents = latents.sample()
+            latents = latents * 0.18215
+            batch_size = pixel_values.shape[0]
+            video_length = pixel_values.shape[1]
+        del pixel_values, batch
+
+        zero_rank_print("Get text embedding", LogType.debug)
+        zero_rank_print(f"Texts {texts}")
+        prompt_ids = tokenizer(
+            texts, max_length=tokenizer.model_max_length, padding="max_length", truncation=True, return_tensors="pt"
+        ).input_ids.to(unet.device)
+        encoder_hidden_states = text_encoder(prompt_ids, output_hidden_states=True).last_hidden_state
+        del texts, prompt_ids
+
+        zero_rank_print("Sample noise", LogType.debug)
+        noise, noisy_latents, timesteps, lookahead_denoising = add_noise_same_timestep(noise_scheduler, latents, batch_size, video_length, generator=train_generator, device=unet.device)
+        zero_rank_print(f"Lookahead {lookahead_denoising}")
+        zero_rank_print(f"noise {noise.shape}", LogType.debug) #[2, 4, 16, 32, 32]
+        zero_rank_print(f"latents {latents.shape}", LogType.debug)
+        zero_rank_print(f"timesteps {timesteps.shape}", LogType.debug) # 32
+        zero_rank_print(f"noisy_latents {noisy_latents.shape}", LogType.debug)
+
+
+    # https://arxiv.org/pdf/2405.20494
+    # perturbation_deviation = embedding_perturbation / math.sqrt(encoder_hidden_states.shape[2])
+    # perturbation_delta =  torch.randn_like(encoder_hidden_states) * (perturbation_deviation)
+    # encoder_hidden_states = encoder_hidden_states + perturbation_delta
+
+        if noise_scheduler.config.prediction_type == "epsilon":
+            target = noise
+        elif noise_scheduler.config.prediction_type in ["v_prediction", "v-prediction"]:
+            target = noise_scheduler.get_velocity(latents, noise, timesteps)
+        else:
+            raise ValueError(f"Unknown prediction type {noise_scheduler.config.prediction_type}")
+        del noise, latents
+
+    with torch.autocast('cuda', enabled=mixed_precision_training):
+        model_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
+
+    if return_loss:
+        if loss_scale is None:
+            loss_scale = torch.ones(model_pred.shape[0], dtype=torch.float)
+
+        # if args.min_snr_gamma is not None:
+        #     snr = compute_snr(timesteps, noise_scheduler)
+
+        #     mse_loss_weights = (
+        #             torch.stack(
+        #                 [snr, args.min_snr_gamma * torch.ones_like(timesteps)], dim=1
+        #             ).min(dim=1)[0]
+        #             / snr
+        #     )
+        #     mse_loss_weights[snr == 0] = 1.0
+        #     loss_scale = loss_scale * mse_loss_weights.to(loss_scale.device)
+        if lookahead_denoising:
+            mid_point = model_pred.shape[2] // 2
+            loss = F.mse_loss(model_pred[:, :, mid_point:, :, :].float(), target[:, :, mid_point:, :, :].float(), reduction="none")
+        else:
+            loss = F.mse_loss(model_pred.float(), target.float(), reduction="none")
+
+        loss_scale = loss_scale.view(-1, 1, 1, 1).expand_as(loss_mse)
+
+        # if args.loss_type == "mse_huber":
+        #     early_timestep_bias = (timesteps / noise_scheduler.config.num_train_timesteps)
+        #     early_timestep_bias = torch.tensor(early_timestep_bias, dtype=torch.float).to(unet.device)
+        #     early_timestep_bias = early_timestep_bias.view(-1, 1, 1, 1).expand_as(loss_mse)
+        #     loss_huber = F.huber_loss(model_pred.float(), target.float(), reduction="none", delta=1.0)
+        #     loss_mse = loss_mse * loss_scale.to(unet.device) * early_timestep_bias
+        #     loss_huber = loss_huber * loss_scale.to(unet.device) * (1.0 - early_timestep_bias)
+        #     loss = loss_mse.mean() + loss_huber.mean()
+        # elif args.loss_type == "huber_mse":
+        #     early_timestep_bias = (timesteps / noise_scheduler.config.num_train_timesteps)
+        #     early_timestep_bias = torch.tensor(early_timestep_bias, dtype=torch.float).to(unet.device)
+        #     early_timestep_bias = early_timestep_bias.view(-1, 1, 1, 1).expand_as(loss_mse)
+        #     loss_huber = F.huber_loss(model_pred.float(), target.float(), reduction="none", delta=1.0)
+        #     loss_mse = loss_mse * loss_scale.to(unet.device) * (1.0 - early_timestep_bias)
+        #     loss_huber = loss_huber * loss_scale.to(unet.device) * early_timestep_bias
+        #     loss = loss_huber.mean() + loss_mse.mean()
+        # elif args.loss_type == "huber":
+        #     loss_huber = F.huber_loss(model_pred.float(), target.float(), reduction="none", delta=1.0)
+        #     loss_huber = loss_huber * loss_scale.to(unet.device)
+        #     loss = loss_huber.mean()
+        # else:
+        loss_mse = loss_mse * loss_scale.to(unet.device)
+        loss = loss_mse.mean()
+
+        return model_pred, target, loss
+
+    else:
+        return model_pred, target
+
 def normalize_and_rescale(image):
     MEAN = [0.5, 0.5, 0.5]
     SD = [0.5, 0.5, 0.5]
@@ -55,25 +160,25 @@ def normalize_and_rescale(image):
     std = torch.tensor(SD).view(1, 1, 3, 1, 1).to(image.device)
     return (image.float()/255.0 - mean) / std
 
-def add_noise_same_timestep(noise_scheduler, latents, batch_size, video_length, generator):
+def add_noise_same_timestep(noise_scheduler, latents, batch_size, video_length, generator, device):
     latents = rearrange(latents, "(b f) c h w -> b c f h w", f=video_length)
     # Sample noise that we'll add to the latents
     noise = randn_tensor(latents.shape, generator=generator, device=latents.device, dtype=latents.dtype)
-    timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (batch_size,), device=device_id).long()
+    timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (batch_size,), device=device).long()
     noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
     return noise, noisy_latents, timesteps, False
 
-def add_noise_random_timesteps(noise_scheduler, latents, batch_size, video_length, generator):
+def add_noise_random_timesteps(noise_scheduler, latents, batch_size, video_length, generator, device):
     # Sample noise that we'll add to the latents
     noise = randn_tensor(latents.shape, generator=generator, device=latents.device, dtype=latents.dtype)
-    timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (batch_size * video_length,), device=device_id).long()
+    timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (batch_size * video_length,), device=device).long()
     noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
     noisy_latents = rearrange(noisy_latents, "(b f) c h w -> b c f h w", f=video_length)
     noise = rearrange(noise, "(b f) c h w -> b c f h w", f=video_length)
     timesteps = rearrange(timesteps, '(b f) -> b f', f=video_length)
     return noise, noisy_latents, timesteps, False
 
-def add_noise_sequential_timesteps(noise_scheduler, latents, batch_size, video_length, generator):
+def add_noise_sequential_timesteps(noise_scheduler, latents, batch_size, video_length, generator, device):
     target_partitions = 2
     target_steps = video_length * target_partitions #32
 
@@ -86,7 +191,7 @@ def add_noise_sequential_timesteps(noise_scheduler, latents, batch_size, video_l
     rank = random.randint(0, 2 * target_partitions - 1)
     start_idx = rank*(video_length // 2)
     end_idx = start_idx + video_length
-    timesteps = timesteps[start_idx:end_idx].to(device_id).long()
+    timesteps = timesteps[start_idx:end_idx].to(device).long()
     print(f"timesteps {timesteps}")
 
     noise = randn_tensor(latents.shape, generator=generator, device=latents.device, dtype=latents.dtype)
@@ -272,27 +377,11 @@ def train_ad(
         )
 
     # Move models to GPU
-
-    # unet_dtype, tenc_dtype, vae_dtype = get_model_dtypes(device_id, force_half)
-    # model_memory_format = get_memory_format(device_id)
-
-    # text_encoder = text_encoder.to(device=device_id, dtype=tenc_dtype)
-    # vae = vae.to(device=device_id, dtype=vae_dtype, memory_format=model_memory_format)
-
     text_encoder = text_encoder.to(device=device_id)
     vae = vae.to(device=device_id)
 
     # Get the training dataset
     train_dataloader = make_dataloader(**train_data, batch_size=train_batch_size, num_workers=num_workers, epoch_size=epoch_size*train_batch_size*gradient_accumulation_steps, seed=seed)
-
-    # Get the training iteration
-    # if max_train_steps == -1:
-    #     assert max_train_epoch != -1
-    #     max_train_steps = max_train_epoch * len(train_dataset)
-
-    # if checkpointing_steps == -1:
-    #     assert checkpointing_epochs != -1
-    #     checkpointing_steps = checkpointing_epochs * len(train_dataset)
 
     if scale_lr:
         learning_rate = (learning_rate * gradient_accumulation_steps * train_batch_size * num_processes)
@@ -331,6 +420,9 @@ def train_ad(
     # Support mixed-precision training
     scaler = torch.amp.GradScaler('cuda') if mixed_precision_training else None
 
+    loss_epoch = []
+    loss_log_step = []
+
     unet.train()
     for epoch in range(first_epoch, num_epochs):
         epoch_loss = 0
@@ -340,7 +432,7 @@ def train_ad(
 
             pixel_values = batch[0].to(device_id)
             texts = batch[1]
-            start_time = batch[2]
+
 
             pixel_values = normalize_and_rescale(pixel_values)
             # zero_rank_print(f"Pixel shape {pixel_values}")
@@ -424,74 +516,11 @@ def train_ad(
                 torch.cuda.empty_cache()
                 gc.collect()
 
-            zero_rank_print("Convert videos to latent space", LogType.debug)
-            # Convert videos to latent space
-            # pixel_values = batch["pixel_values"].to(device_id, dtype=vae_dtype, memory_format=model_memory_format)
-            batch_size = pixel_values.shape[0]
-            video_length = pixel_values.shape[1]
-            with torch.no_grad():
-                if not image_finetune:
-                    pixel_values = rearrange(pixel_values, "b f c h w -> (b f) c h w")
-                    # pixel_values = pixel_values.to(device_id, dtype=vae_dtype, memory_format=model_memory_format)
-                    latents = vae.encode(pixel_values).latent_dist
-                    latents = latents.sample()
-                    # latents = rearrange(latents, "(b f) c h w -> b c f h w", f=video_length)
-                else:
-                    # pixel_values = pixel_values.to(device_id, dtype=vae_dtype, memory_format=model_memory_format)
-                    latents = vae.encode(pixel_values).latent_dist
-                    latents = latents.sample()
 
-                latents = latents * 0.18215
 
-            zero_rank_print("Sample noise", LogType.debug)
-            # noise, noisy_latents, timesteps, lookahead_denoising = add_noise_random_timesteps(noise_scheduler, latents, batch_size, video_length, generator=train_generator)
-            noise, noisy_latents, timesteps, lookahead_denoising = add_noise_same_timestep(noise_scheduler, latents, batch_size, video_length, generator=train_generator)
-            # noise, noisy_latents, timesteps, lookahead_denoising = add_noise_sequential_timesteps(noise_scheduler, latents, batch_size, video_length, generator=train_generator)
-            zero_rank_print(f"Lookahead {lookahead_denoising}")
+            model_pred, target, loss = get_model_prediction_and_target(batch, unet, vae, noise_scheduler, tokenizer, text_encoder, return_loss=True, mixed_precision_training=mixed_precision_training, image_finetune=image_finetune)
 
-            zero_rank_print(f"noise {noise.shape}", LogType.debug) #[2, 4, 16, 32, 32]
-            zero_rank_print(f"latents {latents.shape}", LogType.debug)
-            zero_rank_print(f"timesteps {timesteps.shape}", LogType.debug) # 32
-            zero_rank_print(f"noisy_latents {noisy_latents.shape}", LogType.debug)
-
-            zero_rank_print("Get text embedding", LogType.debug)
-            # Get the text embedding for conditioning
-            zero_rank_print(f"Texts {texts}")
-            with torch.no_grad():
-                prompt_ids = tokenizer(
-                    texts, max_length=tokenizer.model_max_length, padding="max_length", truncation=True, return_tensors="pt"
-                ).input_ids.to(device_id)
-                encoder_hidden_states = text_encoder(prompt_ids)[0]
-
-            del latents, batch
-            torch.cuda.empty_cache()
-
-            # Get the target for loss depending on the prediction type
-            if noise_scheduler.config.prediction_type == "epsilon":
-                target = noise
-            elif noise_scheduler.config.prediction_type == "v_prediction":
-                raise NotImplementedError
-            else:
-                raise ValueError(f"Unknown prediction type {noise_scheduler.config.prediction_type}")
-
-            # Reset gradients at the beginning of the accumulation cycle
-            if step % gradient_accumulation_steps == 0:
-                zero_rank_print(f"Reset gradients at the beginning of the accumulation cycle", LogType.debug)
-                optimizer.zero_grad()
-
-            # Predict the noise residual and compute loss
-            # Mixed-precision training
-            zero_rank_print(f"Predict the noise residual and compute loss Mixed Precision: {mixed_precision_training}", LogType.debug)
-            with torch.amp.autocast('cuda', enabled=mixed_precision_training):
-                model_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
-                if lookahead_denoising:
-                    mid_point = model_pred.shape[2] // 2
-                    loss = F.mse_loss(model_pred[:, :, mid_point:, :, :].float(), target[:, :, mid_point:, :, :].float(), reduction="mean")
-                else:
-                    loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
-                loss = loss / gradient_accumulation_steps  # Normalize loss to account for accumulation
-
-            del noisy_latents, noise, timesteps, model_pred
+            del target, model_pred
             torch.cuda.empty_cache()
 
             # Backpropagate loss, accumulate gradients
@@ -500,6 +529,9 @@ def train_ad(
                 scaler.scale(loss).backward()  # Scale the loss; backward pass accumulates gradients
             else:
                 loss.backward()  # Gradient accumulation without scaling
+
+            loss_step = loss.detach().item()
+            loss_log_step.append(loss_step)
 
             # Apply the optimizer step and update the learning rate scheduler only at the end of an accumulation period
             if (step + 1) % gradient_accumulation_steps == 0:
@@ -520,26 +552,29 @@ def train_ad(
                 sample_end_time = time.time()
                 sample_time = sample_end_time - sample_start_time
                 # Log to WandB
+                loss_step = sum(loss_log_step) / len(loss_log_step)
+                loss_log_step = []
                 if is_main_process and step > 1 and (not is_debug) and use_wandb:
                     wandb.log({
-                        "train_loss": loss.item() * gradient_accumulation_steps,
+                        "train_loss": loss_step,
                         "lr": lr_scheduler.get_lr()[0],
                         "epoch": actual_steps // checkpointing_steps,
                         "sample_time": sample_time,
-                        "data_wait_time": data_wait_time,
-                        "start_time": start_time
-                    })
+                        "data_wait_time": data_wait_time
+                    }, step=global_step)
                     zero_rank_print(f"train_loss {loss.item() * gradient_accumulation_steps} epoch {epoch} sample_time {sample_time} data_wait_time {data_wait_time}", LogType.debug)
                 epoch_loss += loss.item() * gradient_accumulation_steps
+
+                zero_rank_print(f"Reset gradients at the beginning of the accumulation cycle", LogType.debug)
+                optimizer.zero_grad()
+            del loss
+            torch.cuda.empty_cache()
 
             # Update the progress bar
             progress_bar.set_postfix(loss=epoch_loss / (step + 1))
             progress_bar.update()
             global_step += 1
             ### <<<< Training <<<< ###
-
-            del loss
-            torch.cuda.empty_cache()
 
             # Save checkpoint
             print(f"Actual steps {actual_steps} {actual_steps % checkpointing_steps}")
